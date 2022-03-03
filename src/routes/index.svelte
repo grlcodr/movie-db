40b4c1c2e12568b0b124589049025e234b4cf03b
grlcodr <rachael@grlcodr.dev>
2022-03-03 20:23:21 +0000

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+    export async function load({fetch}){
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f82084b0ae7fedc85d09a83f06f8baa1&language=en-US&page=1`);
+        const data = await res.json();
+        console.log(data)
+        if (res.ok){
+            return {
+                props: { popular: data.results }
+            };
+        }
+    }
+</script>
+
+<script>
+    import PopularMovies from '../components/PopularMovies.svelte';
+    export let popular;
+</script>
+
+<section>
+    <PopularMovies {popular}/>
+</section>
+
